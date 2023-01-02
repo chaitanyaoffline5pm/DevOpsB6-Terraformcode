850898f369f65554695e711a8b98cb4e6543c8ac
chaitanya <chaitanya@gmail.com>
2023-01-02 17:31:41 +0530

    Credntials added

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  
+  access_key = "AKIAWQJOIUUFX5TZ57QH"
+  secret_key = "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"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "AwsB6"{
-    cidr_block = "10.100.0.0/16"
-    enable_dns_hostnames=true
-    tags = {
-        Name = "AwsB6-vpc"
-        Env ="Dev"
-    }
+resource "aws_vpc" "AwsB6" {
+  cidr_block           = "10.100.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "AwsB6-vpc"
+    Env  = "Dev"
+  }
 }
