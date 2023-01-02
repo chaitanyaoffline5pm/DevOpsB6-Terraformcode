47d97e64e8c7286c83de4670b44db310e434a91e
chaitanya <chaitanya@gmail.com>
2023-01-02 17:38:02 +0530

    added IGW

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,13 @@ resource "aws_vpc" "AwsB6" {
   cidr_block           = "10.100.0.0/16"
   enable_dns_hostnames = true
   tags = {
-    Name = "AwsB6-vpc"
+    Name = "DevOpsB6-vpc"
     Env  = "Dev"
   }
+}
+resource "aws_internet_gateway" "AwsB6-IGw"{
+    vpc_id=aws_vpc.AwsB6.vpc_id
+    tags = {
+        Name = "DevOpsB6-IGW"
+    }
 }
