a56fd3cc9b712c2659b8c64938a062e23b96938a
sysadmin <sysadmin@cgitedu.com>
2023-01-03 11:57:02 +0000

    subnet 3 added

--- subnet.tf
+++ subnet.tf
@@ -16,3 +16,12 @@ resource "aws_subnet" "Public-Subnet-2" {
     Name = var.subnet_name_2
   }
 }
+resource "aws_subnet" "Public-Subnet-3" {
+  cidr_block              = var.subnet_cidr_3
+  vpc_id                  = aws_vpc.AwsB6.id
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = var.subnet_name_3
+  }
+}
--- variables.tf
+++ variables.tf
@@ -6,5 +6,7 @@ variable "subnet_cidr" {}
 variable "subnet_name" {}
 variable "subnet_cidr_2" {}
 variable "subnet_name_2" {}
+variable "subnet_cidr_3" {}
+variable "subnet_name_3" {}
 
 
